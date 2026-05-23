7e04bb5abde85ea88e4c34bd4212e465f7f54d62
BOINI NANDA KUMAR <boininandakumar@gmail.com>
2026-05-23 09:36:48 +0530

    1st Upload

---
+++ app.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+operators = ["+", "-", "*", "/"]
+if "+" :
+    print( a+b )
+elif "-" :
+    print( a-b )
+elif "*" :
+    print( a*b )
+else :
+    print( a/b )    
